bba334c8d59ceca25c78fcaa486387b834208802
Shizuka murakami <69420652+omuomu02414@users.noreply.github.com>
2020-08-11 10:05:52 +0900

    Add files via upload

---
+++ training/js/main.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    new Swiper('.swiper-container', {
+        loop: true,
+        autoHeight: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: true
+        },
+        speed: 500,
+        effect: 'fade',    
+        });
+}
